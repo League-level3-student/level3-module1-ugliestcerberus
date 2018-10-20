a77b811611b65756d1c87825df9997f6b8f38c1f
ugliestcerberus <league@WTS5.attlocal.net>
2018-10-20 14:27:30 -0700

    IntroToArrayLists(ip), DynamicIntegerArray

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -2,15 +2,22 @@ package _00_Dynamic_Integer_Array;
 
 public class DynamicIntegerArray {
 	// 1. Create a private int array. Don't initialize it.
+	// 2. Initialize the int array to have 0 elements.
+	// This will prevent a null pointer exception
 	private int[] yes = new int[0];
 
 	public DynamicIntegerArray() {
-		// 2. Initialize the int array to have 0 elements.
-		// This will prevent a null pointer exception
-
+		
 	}
-
 	// 3. Complete the steps in the add method
+		// A. create and initialize a new int array to be one
+		// element longer than the member array
+		// B. set the last element of your new array to
+		// the value passed into the method
+		// C. iterate through the member array and
+		// copy every element from the member array
+		// to the new array
+		// D. set the member array equal to the new array.
 	public void add(int v) {
 		int[] no = new int[0];
 		for (int n = 0; n < 9; n++) {
@@ -22,24 +29,16 @@ public class DynamicIntegerArray {
 		System.out.println(no[no.length - 1]);
 		for (int i = 0; i < no.length; i++) {
 			System.out.println(no[i]);
-			;
 		}
 
 		yes = no;
-		// A. create and initialize a new int array to be one
-		// element longer than the member array
-		// B. set the last element of your new array to
-		// the value passed into the method
-		// C. iterate through the member array and
-		// copy every element from the member array
-		// to the new array
-		// D. set the member array equal to the new array.
+
 	}
 
 	// 4. Complete the steps in the get method
+	// A. Return the value of the memeber array at the location passed in
 	public int get(int location) {
 		return location;
-		// A. Return the value of the memeber array at the location passed in
 	}
 
 	// 5. Run the DynamicArrayTest to see if you are correct so far.
@@ -48,12 +47,20 @@ public class DynamicIntegerArray {
 	}
 
 	// 6. Complete the steps in the set method
+	// A. set the variable at the location passed in to the method
+	// to the new value v
 	public void set(int v, int location) {
 		location = v;
-		// A. set the variable at the location passed in to the method
-		// to the new value v
 	}
 
+		//7. Complete the steps in the insert method
+		//A. create and initialize a new int array to be one element longer than the member array
+		//B. Make a for loop that iterates through the new array
+		//C. if i is less than location, set the element at i of the new array to the element at i of the member array
+		//D. else if i is greater than location, set the element at i of the new array to the i - 1 element of the member array
+		//E. else, set the element at i of the new array to the value v
+		//F. set the member array equal to the new array	
+	
 	public void insert(int v, int location) {
 		int[]g= new int[10];
 		for (int h = 0; h < g.length; h++) {
@@ -67,37 +74,18 @@ public class DynamicIntegerArray {
 		else{
 		g[h]=v;	
 		}
-		=g[h];
-		//g=h;
+		//yes=h;
+		yes=g;
 		}
-		//7. Complete the steps in the insert method
-		//A. create and initialize a new int array to be one element longer than the member array
-		//B. Make a for loop that iterates through the new array
-		//C. if i is less than location, set the element at i of the new array to the element at i of the member array
-		//D. else if i is greater than location, set the element at i of the new array to the i - 1 element of the member array
-		//E. else, set the element at i of the new array to the value v
-		//F. set the member array equal to the new array
-	}
 
+		//8. Run the tests again and check your progress	
+	}
+	void drawall() {
 	insert(10,10);
 	set(20,20);
-	get(100,100);
+	get(100);
+	}
 	
-	//8. Run the tests again and check your progress
-
-	public void remove(int location) {
-		int[] n =new int[9];
-		for (int m = 0; m > n.length; m++) {
-			if(m<location) {
-				n=m;
-			}
-		}
-		else if(){
-			
-		} 
-		{
-		n=m;	
-		}
 		//9. Complete the steps in the remove method
 		//A. create a new array that is one element smaller than the member array
 		//B. make a for loop to iterate through the member array
@@ -108,19 +96,39 @@ public class DynamicIntegerArray {
 			//E. else, continue;
 			
 		//F. set the member array equal to the new array
+	
+	public void remove(int location) {
+		int[] n =new int[9];
+		for (int m = 0; m > n.length; m++) {
+		if(m<location) {
+			n[m]++;
+		}
+		else if(m>location){
+			n[m]-=1;
+		}
+		else {
+			continue;
+		}
+		} 
+		
+		yes=n;
 	}
+
+	
 
 	// 10. Run the tests again the see if you are correct so far
 
 	// 11. Complete the size method so that it returns the length of the member
 	// array.
 	public int size() {
-		return 0;
+		return yes.length;
 	}
-
+	
 	// 12. Complete the clear array so that it sets the member array
 	// equal to a new integer array of size 0
 	public void clear() {
+		int[] clear= new int[0]; 
+		
 	}
 
 	// 13. Run the test again to see if you are finished.
--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -6,18 +6,31 @@ public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-
 		//2. Add five Strings to your list
-		
+		String[] sayings= {"awesome","realistic","magnificient","lethargic", "energetic"};
 		//3. Print all the Strings using a standard for-loop
-		
-		
+		for(int u = 0; u< 5; u++) {
+			System.out.println(sayings[u]);
+		}
 		//4. Print all the Strings using a for-each loop
-		
+		for(int h= 0; h< 5; h++) {
+			//System.out.println(sayings);
+			System.out.println(sayings[h]);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for(int g=0;g<5;g+=2) {
+			//System.out.println(sayings);
+			System.out.println(sayings[g]);
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for(int j=5; j<-1; j--) {
+			//System.out.println(sayings);
+			System.out.println(sayings[j]);
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		//System.out.println(x);
+		//sayings.charAt(2);
+			
+		}
 	}
-}
+
