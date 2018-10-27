6664212809bf2928006344dc84e055ddd8716545
trygamer <league@WTS14-4.attlocal.net>
2018-10-27 14:24:46 -0700

    IntroToArrayLists, GuestBook(ip)

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -4,33 +4,52 @@ import java.util.ArrayList;
 
 public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
-		//2. Add five Strings to your list
-		String[] sayings= {"awesome","realistic","magnificient","lethargic", "energetic"};
-		//3. Print all the Strings using a standard for-loop
-		for(int u = 0; u< 5; u++) {
-			System.out.println(sayings[u]);
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		// 2. Add five Strings to your list
+		ArrayList<String> sayings = new ArrayList<String>();
+		sayings.add("Matthew is awesome");
+		sayings.add("Reading, Writing, Math");
+		sayings.add("I like food");
+		sayings.add("I want to sleep");
+		System.out.println("next");
+		// sayings.remove(0);
+		// This code is there to show that this is very similar to the concepts of
+		// understanding arrays.
+		// System.out.println(sayings.get(0));
+		// This code is there to show that this is very similar to the concepts of
+		// understanding arrays, too.
+		// This code shows which element of the arraylist shall be printed.
+		// The first element starts with zero and the number element increases by one
+		// when we add a element.
+		// parenthesis is for setting up the contructor
+		// 3. Print all the Strings using a standard for-loop
+		for (int u = 0; u < sayings.size(); u++) {
+			System.out.println(sayings.get(u));
 		}
-		//4. Print all the Strings using a for-each loop
-		for(int h= 0; h< 5; h++) {
-			//System.out.println(sayings);
-			System.out.println(sayings[h]);
+		System.out.println("nest");
+		// 4. Print all the Strings using a for-each loop
+		for (String s : sayings) {
+			System.out.println(s);
 		}
-		//5. Print only the even numbered elements in the list.
-		for(int g=0;g<5;g+=2) {
-			//System.out.println(sayings);
-			System.out.println(sayings[g]);
+		System.out.println("then");
+		// 5. Print only the even numbered elements in the list.
+		for (int g = 0; g < sayings.size(); g += 2) {
+			// System.out.println(sayings);
+			System.out.println(sayings.get(g));
 		}
-		//6. Print all the Strings in reverse order.
-		for(int j=5; j<-1; j--) {
-			//System.out.println(sayings);
-			System.out.println(sayings[j]);
+		System.out.println("two more");
+		// 6. Print all the Strings in reverse order.
+		for (int j = sayings.size() - 1; j >= 0; j--) {
+			System.out.println(sayings.get(j));
 		}
-		//7. Print only the Strings that have the letter 'e' in them.
-		//System.out.println(x);
-		//sayings.charAt(2);
-			
+		System.out.println("Last");
+		// 7. Print only the Strings that have the letter 'e' in them.
+		for (int m = 0; m < sayings.size(); m++) {
+			if (sayings.get(m).contains("e")) {
+				System.out.println(sayings.get(m));
+			}
 		}
+		System.out.println("e");
 	}
-
+}
--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,14 +1,24 @@
 package _01_IntroToArrayLists;
 
+import javax.swing.JButton;
+
 public class _02_GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
+	// 1.Create a GUI with two buttons. 
+	JButton jbutton= new JButton();	
+	JButton jbutton2= new JButton();
+	// 2.One button reads "Add Name" and the other button reads "View Names".
+	jbutton.setText("Add Name");
+	jbutton2.setText("View Names");
+	// 3.When the add name button is clicked, display an input dialog that asks the user to enter a name. 
+	// 4.Add that name to an ArrayList.
+	// 5.When the "View Names" button is clicked, display a message dialog that displays all the names added to the list. 
+	// 6.Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	
+	
 	
 	
 	
