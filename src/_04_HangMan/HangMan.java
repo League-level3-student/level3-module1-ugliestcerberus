fc8af5e4982c2002dc13c28b4928f622da52864b
ugliestcerberus <league@iMac-5.attlocal.net>
2019-04-20 14:28:57 -0700

    Hangman(ip)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -7,16 +7,20 @@ import java.util.Stack;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class HangMan implements KeyListener{
-	Stack math= new Stack();
-	String duplicates= "";
+	Stack <String> math= new Stack<String>();
 	JLabel jlabel= new JLabel();
 	JFrame jframe= new JFrame();
+	JPanel jpanel= new JPanel();
+	int numberoflives= 10;
+	String text= "_____________________";
+	//dictionary
 	
 	//KeyListener has= new KeyListener();
 public static void main(String[]args) {	
-	Utilities.readRandomLineFromFile("dictionary.txt");
+	
 	HangMan matthew= new HangMan();
 	matthew.createUI();
 	//Utilities.readRandomLineFromFile();
@@ -25,32 +29,39 @@ public static void main(String[]args) {
 void createUI(){
 jframe.setVisible(true);
 jframe.add(jlabel);
- 	math.push("dictionary.txt");
- 	jframe.setSize(1000,1000);
-//math.contains(dictionary.txt);	 
- }
+jframe.setSize(1000,1000);
+jframe.add(jpanel);
+String mass= JOptionPane.showInputDialog("How many words do you want?");
+int numberoftimes=Integer.parseInt(mass);
+for(int y= 0; y<numberoftimes; y++) {
+	math.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+}
+math.contains(true);
+//dictionary.txt();
+//math.contains(dictionary.txt);	
 
-public void keyTyped(KeyEvent e) {
-	
-	
 }
 
+public void keyTyped(KeyEvent e) {
+		
+}
 public void keyPressed(KeyEvent e) {
-	//if(e.getSource()) {
-		jlabel.show();
+	//if(e.getSource()) {	
 	//}
+jlabel.setText(math.pop());		
+String letters= JOptionPane.showInputDialog("Please type in some letters");
+if(tes) {
 	
+}
+//1. Type in some code that pops out the words 2. collect some inputs 3. if statement whether the key words are correct
 }
 
 public void keyReleased(KeyEvent e) {
 	
-	
-
-
 for(int t=0; t<10; t++) {
 	
 }
-String mass= JOptionPane.showInputDialog("How many words do you want?");
+
 
 }
 
