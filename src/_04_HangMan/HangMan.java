a1bd44b16e2cc30f5b200fef94aafc8818e37f1b
Joyce Garofalo <garofalo.je@gmail.com>
2019-05-04 14:30:00 -0700

    Hangman(ip)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -12,13 +12,13 @@ import javax.swing.JPanel;
 public class HangMan implements KeyListener{
 	Stack <String> math= new Stack<String>();
 	JLabel jlabel= new JLabel();
+	JLabel jlabel2= new JLabel();
 	JFrame jframe= new JFrame();
 	JPanel jpanel= new JPanel();
 	int numberoflives= 10;
 	String text= "";
-	//dictionary
+	String word;
 	
-	//KeyListener has= new KeyListener();
 public static void main(String[]args) {	
 	
 	HangMan matthew= new HangMan();
@@ -32,15 +32,18 @@ jframe.add(jlabel);
 jframe.setSize(1000,1000);
 jframe.add(jpanel);
 jpanel.add(jlabel);
+jpanel.add(jlabel2);
+jlabel2.setText("number of lives:10");
 //jlabel.setText("lives");
 String mass= JOptionPane.showInputDialog("How many words do you want?");
 int numberoftimes=Integer.parseInt(mass);
 math.push(Utilities.readRandomLineFromFile("dictionary.txt"));
-String word= math.pop();
+word= math.pop();
 for(int y= 0; y<word.length(); y++) {
 text +="_";
 }
 jlabel.setText(text) ;
+jframe.addKeyListener(this);
 
 
 }
@@ -49,14 +52,19 @@ public void keyTyped(KeyEvent e) {
 		
 }
 public void keyPressed(KeyEvent e) {
-	for(int y= 0; y<word.length(); y++) {
-		if(e.getKeyChar()==text) {
-		System.out.println(word);
+	StringBuilder sb= new StringBuilder(jlabel.getText());
+	char pressed= e.getKeyChar();
+	for(int y= 0; y<text.length(); y++) {
+	if(word.charAt(y)==pressed){
+		sb.setCharAt(y,pressed);
 	}
-		}
-	
+	else {
+		numberoflives -=1;
+	}
+	}
+	jlabel.setText(sb.toString());
 }
-}
+
 public void keyReleased(KeyEvent e) {
 	
 
