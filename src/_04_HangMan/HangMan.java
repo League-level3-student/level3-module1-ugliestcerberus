241bda989d6b1bd6cdd44056643c5d4c93804e32
League Student <league@AdministorsiMac.attlocal.net>
2019-05-11 14:29:10 -0700

    Hangman(ip)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -2,6 +2,7 @@ package _04_HangMan;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -13,10 +14,12 @@ public class HangMan implements KeyListener{
 	Stack <String> math= new Stack<String>();
 	JLabel jlabel= new JLabel();
 	JLabel jlabel2= new JLabel();
+	
 	JFrame jframe= new JFrame();
 	JPanel jpanel= new JPanel();
 	int numberoflives= 10;
 	String text= "";
+	ArrayList<Character> letterbank= new ArrayList<Character>();
 	String word;
 	
 public static void main(String[]args) {	
@@ -33,7 +36,7 @@ jframe.setSize(1000,1000);
 jframe.add(jpanel);
 jpanel.add(jlabel);
 jpanel.add(jlabel2);
-jlabel2.setText("number of lives:10");
+jlabel2.setText("number of lives:" + numberoflives);
 //jlabel.setText("lives");
 String mass= JOptionPane.showInputDialog("How many words do you want?");
 int numberoftimes=Integer.parseInt(mass);
@@ -54,15 +57,29 @@ public void keyTyped(KeyEvent e) {
 public void keyPressed(KeyEvent e) {
 	StringBuilder sb= new StringBuilder(jlabel.getText());
 	char pressed= e.getKeyChar();
+	JLabel jlabel3= new JLabel();
 	for(int y= 0; y<text.length(); y++) {
 	if(word.charAt(y)==pressed){
 		sb.setCharAt(y,pressed);
+	if(word.equals(sb)) {
+		JOptionPane.showMessageDialog(null,"You win!");
+		JOptionPane.showConfirmDialog(null,"Wanna play again");
 	}
-	else if(numberoflives==0) {
+	}
+	
+	else if(word.charAt(y)!=pressed && !letterbank.contains(pressed)) {
+		System.out.println(numberoflives -= 1);
+		jlabel2.setText("number of lives:" + numberoflives);
+		letterbank.add(pressed);
+	}
+	else if(numberoflives==0){
+		JOptionPane.showMessageDialog(null,"GameOver");
+		JOptionPane.showMessageDialog(null, "Here is the word" + word);
+		int integer= JOptionPane.showConfirmDialog(null, "Wanna Play again?");
+		if(integer==1) {
+		System.exit(0);	
+		}
 		
-	}
-	else {
-		numberoflives -=1;
 	}
 	}
 	jlabel.setText(sb.toString());
